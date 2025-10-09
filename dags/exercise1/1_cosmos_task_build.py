f70b231dd8eb5f182abee3f51046d50ee1504592
Tatiana Al-Chueyr <tatiana.alchueyr@gmail.com>
2025-10-09 05:00:14 -0700

    Add scheleton DAGs for exercise 1 DAGs

---
+++ dags/exercise1/1_cosmos_task_build.py
@@ -0,0 +1,28 @@
+"""
+Airflow DAG to run a Jaffle Shop dbt project using Cosmos.
+from airflow import DAG
+"""
+from pathlib import Path
+
+from airflow.sdk import DAG
+from cosmos import DbtBuildLocalOperator, ProfileConfig
+
+
+DBT_PROJECT_DIR = Path(__file__).parent.parent.parent / "dbt/jaffle_shop"
+DBT_BIN_PATH = "dbt"
+PROFILE_NAME = "bq_profile"
+TARGET_NAME = "dev"
+PROFILE_YML_FILEPATH = DBT_PROJECT_DIR / "profiles.yml"
+
+
+profile_config = ProfileConfig(
+    profile_name=PROFILE_NAME,
+    target_name=TARGET_NAME,
+    profiles_yml_filepath=PROFILE_YML_FILEPATH,
+)
+
+# TODO: Complete the code below with the correct parameters and values
+#with DAG("1_cosmos_task_build") as dag:
+#    build = DbtBuildLocalOperator(
+#    )
+#    build
---
+++ dags/exercise1/1_cosmos_task_multi.py
@@ -0,0 +1,31 @@
+"""
+Airflow DAG to run a Jaffle Shop dbt project using Cosmos.
+from airflow import DAG
+"""
+from pathlib import Path
+
+from airflow.sdk import DAG
+from cosmos import DbtRunLocalOperator, DbtSeedLocalOperator, DbtTestLocalOperator, ProfileConfig
+
+
+DBT_PROJECT_DIR = Path(__file__).parent.parent.parent / "dbt/jaffle_shop"
+DBT_BIN_PATH = "dbt"
+PROFILE_NAME = "bq_profile"
+TARGET_NAME = "dev"
+PROFILE_YML_FILEPATH = DBT_PROJECT_DIR / "profiles.yml"
+
+
+profile_config = ProfileConfig(
+    profile_name=PROFILE_NAME,
+    target_name=TARGET_NAME,
+    profiles_yml_filepath=PROFILE_YML_FILEPATH,
+)
+
+
+# TODO: Complete the code below with the correct parameters and values
+#with DAG("solution_1_cosmos_task_multi") as dag:
+#    seed = DbtSeedLocalOperator(
+#
+#    )
+#    seed >> run >> test
+
