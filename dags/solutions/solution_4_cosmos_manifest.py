b7c4e933681c1272fcdea9e43f0120cdb13e052c
Tatiana Al-Chueyr <tatiana.alchueyr@gmail.com>
2025-10-09 09:51:53 -0700

    Add exercise 4 solutions

---
+++ dags/solutions/solution_4_cosmos_manifest.py
@@ -0,0 +1,38 @@
+
+"""
+Airflow DAG to run a Jaffle Shop dbt project using Cosmos.
+from airflow import DAG
+"""
+from datetime import datetime
+from pathlib import Path
+
+from cosmos import DbtDag, ProjectConfig, ProfileConfig, RenderConfig
+from cosmos.constants import LoadMode
+
+
+DBT_PROJECT_DIR = Path(__file__).parent.parent.parent / "dbt/jaffle_shop"
+PROFILE_NAME = "bq_profile"
+TARGET_NAME = "dev"
+PROFILE_YML_FILEPATH = DBT_PROJECT_DIR / "profiles.yml"
+DBT_MANIFEST_PATH = str(DBT_PROJECT_DIR / "target/manifest.json")
+
+profile_config = ProfileConfig(
+    profile_name=PROFILE_NAME,
+    target_name=TARGET_NAME,
+    profiles_yml_filepath=PROFILE_YML_FILEPATH,
+)
+
+render_config = RenderConfig(
+    load_method=LoadMode.DBT_MANIFEST,
+)
+
+solution_4_cosmos_manifest = DbtDag(
+    dag_id="solution_4_cosmos_manifest",
+    start_date=datetime(2025, 10, 8),
+    project_config=ProjectConfig(
+        DBT_PROJECT_DIR,
+        manifest_path=DBT_MANIFEST_PATH
+    ),
+    render_config=render_config,
+    profile_config=profile_config,
+)
---
+++ dags/solutions/solution_4_cosmos_test_build.py
@@ -0,0 +1,39 @@
+
+"""
+Airflow DAG to run a Jaffle Shop dbt project using Cosmos.
+from airflow import DAG
+"""
+from datetime import datetime
+from pathlib import Path
+
+from cosmos import DbtDag, ProjectConfig, ProfileConfig, RenderConfig
+from cosmos.constants import LoadMode, TestBehavior
+
+
+DBT_PROJECT_DIR = Path(__file__).parent.parent.parent / "dbt/jaffle_shop"
+PROFILE_NAME = "bq_profile"
+TARGET_NAME = "dev"
+PROFILE_YML_FILEPATH = DBT_PROJECT_DIR / "profiles.yml"
+DBT_MANIFEST_PATH = str(DBT_PROJECT_DIR / "target/manifest.json")
+
+profile_config = ProfileConfig(
+    profile_name=PROFILE_NAME,
+    target_name=TARGET_NAME,
+    profiles_yml_filepath=PROFILE_YML_FILEPATH,
+)
+
+render_config = RenderConfig(
+    load_method=LoadMode.DBT_MANIFEST,
+    test_behavior=TestBehavior.BUILD,
+)
+
+solution_4_cosmos_select = DbtDag(
+    dag_id="solution_4_cosmos_select",
+    start_date=datetime(2025, 10, 8),
+    project_config=ProjectConfig(
+        DBT_PROJECT_DIR,
+        manifest_path=DBT_MANIFEST_PATH
+    ),
+    render_config=render_config,
+    profile_config=profile_config,
+)
