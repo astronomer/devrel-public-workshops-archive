755f29a126c1e3398fe55c3e3df02dc9f69d7e35
Tatiana Al-Chueyr <tatiana.alchueyr@gmail.com>
2025-10-09 12:56:21 -0700

    Fix issue

--- dags/solutions/solution_4_cosmos_manifest.py
+++ dags/solutions/solution_4_cosmos_manifest.py
@@ -14,7 +14,8 @@ DBT_PROJECT_DIR = Path(__file__).parent.parent / "dbt/jaffle_shop"
 PROFILE_NAME = "bq_profile"
 TARGET_NAME = "dev"
 PROFILE_YML_FILEPATH = DBT_PROJECT_DIR / "profiles.yml"
-DBT_MANIFEST_PATH = Path(__file__).parent.parent.parent / "/include/manifest.json"
+DBT_MANIFEST_PATH = Path(__file__).parent.parent.parent / "include/manifest.json"
+breakpoint()
 
 profile_config = ProfileConfig(
     profile_name=PROFILE_NAME,
