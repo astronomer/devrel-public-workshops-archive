2671191a79748bd04453a82d8da7da5bb8150593
Volker Janz <volker.janz@astronomer.io>
2026-01-22 19:46:43 -0800

    Remove etl specific resources

--- include/sql/cleanup.sql
+++ include/sql/cleanup.sql
@@ -6,7 +6,5 @@ DROP TABLE IF EXISTS routes;
 DROP TABLE IF EXISTS customers;
 DROP TABLE IF EXISTS planets;
 
-DROP TABLE IF EXISTS daily_planet_report;
-
 DROP SEQUENCE IF EXISTS payment_id_seq;
 DROP SEQUENCE IF EXISTS booking_id_seq;
